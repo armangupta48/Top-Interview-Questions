2e36a70162b8aecb4f74cafcc59fd99013e7ee48
Arman Gupta <81355091+armangupta48@users.noreply.github.com>
2021-04-24 01:07:48 +0530

    Create House Robber III.cpp

---
+++ House Robber III.cpp
@@ -0,0 +1,51 @@
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root.
+
+Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that all houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+ 
+
+Example 1:
+
+
+Input: root = [3,2,3,null,3,null,1]
+Output: 7
+Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+Example 2:
+
+
+Input: root = [3,4,5,1,3,null,1]
+Output: 9
+Explanation: Maximum amount of money the thief can rob = 4 + 5 = 9.
+ 
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    map<TreeNode*,int> mp;
+    int rob(TreeNode* root) {
+        return helper(root);
+    }
+    int helper(TreeNode* root)
+    {
+        if(!root) return 0;
+        if(mp.count(root)) return mp[root];
+        int total = 0;
+        if(root->left) total+=helper(root->left->left)+helper(root->left->right);
+        if(root->right) total+=helper(root->right->left)+helper(root->right->right);
+        
+        return mp[root]=max(root->val+total,helper(root->left)+helper(root->right));
+    }
+};
+
