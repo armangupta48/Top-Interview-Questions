8acbf102d7404dd7ba551f12b37b41350d963fac
Arman Gupta <81355091+armangupta48@users.noreply.github.com>
2021-05-07 23:10:57 +0530

    Create Construct the Lexicographically Largest Valid Sequence.cpp

---
+++ Construct the Lexicographically Largest Valid Sequence.cpp
@@ -0,0 +1,59 @@
+/*
+Given an integer n, find a sequence that satisfies all of the following:
+
+The integer 1 occurs once in the sequence.
+Each integer between 2 and n occurs twice in the sequence.
+For every integer i between 2 and n, the distance between the two occurrences of i is exactly i.
+The distance between two numbers on the sequence, a[i] and a[j], is the absolute difference of their indices, |j - i|.
+
+Return the lexicographically largest sequence. It is guaranteed that under the given constraints, there is always a solution.
+
+A sequence a is lexicographically larger than a sequence b (of the same length) if in the first position where a and b differ, sequence a has a number greater than the corresponding number in b. For example, [0,1,9,0] is lexicographically larger than [0,1,5,6] because the first position they differ is at the third number, and 9 is greater than 5.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: [3,1,2,3,2]
+Explanation: [2,3,2,1,3] is also a valid sequence, but [3,1,2,3,2] is the lexicographically largest valid sequence.
+Example 2:
+
+Input: n = 5
+Output: [5,3,1,4,3,5,2,4,2]
+*/
+class Solution {
+public:
+    vector<int> constructDistancedSequence(int n) {
+        vector<int> res(2*n-1);
+        vector<bool> vis(n+1,false);
+        bool ans = helper(res,0,vis,n);
+        return res;
+    }
+    
+    bool helper(vector<int>& res,int i,vector<bool>& vis,int n)
+    {
+        if(i==res.size()){return true;}
+        
+        if(res[i]!=0) return helper(res,i+1,vis,n);
+        for(int j = n;j>=1;j--)
+        {
+            if(vis[j]==false)
+            {
+                if(j!=1 && (i+j>=(2*n-1) || res[i+j]!=0)) continue;
+                res[i] = j;
+                if(j!=1) res[i+j] = j;
+                vis[j] = true;
+                if(helper(res,i+1,vis,n))
+                {
+                    return true;
+                }   
+                res[i] = 0;
+                if(j!=1) res[i+j] = 0;
+                vis[j] = false;
+            }
+        }
+        return false;
+    }
+};
+
